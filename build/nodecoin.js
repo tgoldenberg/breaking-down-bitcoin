bc493d51bcd0d587ba86739ff40ac3e2b38e1668
Tom Goldenberg <thomasagoldenberg@gmail.com>
2018-03-13 07:40:08 -0400

    add function to generate Bitcoin wallets

--- build/nodecoin.js
+++ build/nodecoin.js
@@ -102,10 +102,115 @@ module.exports = require("babel-polyfill");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.Hash = Hash;
+exports.getAddress = getAddress;
 exports.makeWallet = makeWallet;
-function makeWallet() {
-  console.log('Hello world');
+
+var _coinstring = __webpack_require__(3);
+
+var _coinstring2 = _interopRequireDefault(_coinstring);
+
+var _crypto = __webpack_require__(4);
+
+var _crypto2 = _interopRequireDefault(_crypto);
+
+var _elliptic = __webpack_require__(5);
+
+var _ripemd = __webpack_require__(6);
+
+var _ripemd2 = _interopRequireDefault(_ripemd);
+
+var _secureRandom = __webpack_require__(7);
+
+var _secureRandom2 = _interopRequireDefault(_secureRandom);
+
+var _jsSha = __webpack_require__(8);
+
+var _jsSha2 = _interopRequireDefault(_jsSha);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var ecdsa = new _elliptic.ec('secp256k1');
+var DEFAULT_VERSION = { public: 0x0, private: 0x80 };
+
+// hash with both SHA-256 and RIPEMD-160 algorithms
+function Hash(msg) {
+  var hash = _crypto2.default.createHash('sha256').update(msg).digest();
+  return new _ripemd2.default().update(hash).digest();
 }
+
+// get a base58 encoded BTC address given a user's public key
+function getAddress(publicKey) {
+  var publicKeyHash = Hash(publicKey);
+  return _coinstring2.default.encode(publicKeyHash, DEFAULT_VERSION.public);
+}
+
+function makeWallet() {
+  var privateKey = _secureRandom2.default.randomBuffer(32); // 32 byte random number
+  console.log('> Private key created: ', privateKey.toString('hex')); // convert to hexadecimal
+
+  // generate public key from private
+  var keys = ecdsa.keyFromPrivate(privateKey);
+  var publicKey = keys.getPublic('hex');
+  console.log('> Public key created: ', publicKey);
+
+  // generate public key hash
+  var publicKeyHash = Hash(publicKey);
+  console.log('> Public key hash created: ', publicKeyHash.toString('hex'));
+
+  // generate public address
+  var publicAddress = _coinstring2.default.encode(publicKeyHash, DEFAULT_VERSION.public);
+  console.log('> Public address created: ', publicAddress);
+
+  // generate private key WIF (wallet import format)
+  var privateKeyWIF = _coinstring2.default.encode(privateKey, DEFAULT_VERSION.private);
+  console.log('> Private key WIF (wallet import format) created : ', privateKeyWIF);
+
+  var wallet = {
+    privateKey: privateKey.toString('hex'),
+    publicKey: publicKey,
+    publicKeyHash: publicKeyHash.toString('hex'),
+    privateKeyWIF: privateKeyWIF,
+    publicAddress: publicAddress
+  };
+  return wallet;
+}
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+module.exports = require("coinstring");
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+module.exports = require("crypto");
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+module.exports = require("elliptic");
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports = require("ripemd160");
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+module.exports = require("secure-random");
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports) {
+
+module.exports = require("js-sha256");
 
 /***/ })
 /******/ ]);
--- src/utils/makeWallet.js
+++ src/utils/makeWallet.js
@@ -1,3 +1,53 @@
+import coinstring from 'coinstring';
+import crypto from 'crypto';
+import { ec } from 'elliptic';
+import ripemd160 from 'ripemd160';
+import secureRandom from 'secure-random';
+import sha256 from 'js-sha256';
+
+const ecdsa = new ec('secp256k1');
+const DEFAULT_VERSION = { public: 0x0, private: 0x80 };
+
+// hash with both SHA-256 and RIPEMD-160 algorithms
+export function Hash(msg) {
+  let hash = crypto.createHash('sha256').update(msg).digest();
+  return new ripemd160().update(hash).digest();
+}
+
+// get a base58 encoded BTC address given a user's public key
+export function getAddress(publicKey) {
+  const publicKeyHash = Hash(publicKey);
+  return coinstring.encode(publicKeyHash, DEFAULT_VERSION.public);
+}
+
+
 export function makeWallet() {
-  console.log('Hello world');
+  const privateKey = secureRandom.randomBuffer(32); // 32 byte random number
+  console.log('> Private key created: ', privateKey.toString('hex')); // convert to hexadecimal
+
+  // generate public key from private
+  const keys = ecdsa.keyFromPrivate(privateKey);
+  const publicKey = keys.getPublic('hex');
+  console.log('> Public key created: ', publicKey);
+
+  // generate public key hash
+  const publicKeyHash = Hash(publicKey);
+  console.log('> Public key hash created: ', publicKeyHash.toString('hex'));
+
+  // generate public address
+  const publicAddress = coinstring.encode(publicKeyHash, DEFAULT_VERSION.public);
+  console.log('> Public address created: ', publicAddress);
+
+  // generate private key WIF (wallet import format)
+  const privateKeyWIF = coinstring.encode(privateKey, DEFAULT_VERSION.private);
+  console.log('> Private key WIF (wallet import format) created : ', privateKeyWIF);
+
+  const wallet = {
+    privateKey: privateKey.toString('hex'),
+    publicKey,
+    publicKeyHash: publicKeyHash.toString('hex'),
+    privateKeyWIF,
+    publicAddress
+  };
+  return wallet;
 }
